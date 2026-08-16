05d4e50685fdd17973a0e7085dc58971864cc282
Jojo.BAT <joaquimdiassepini@gmail.com>
2026-08-16 08:42:02 -0300

    Update version in inc function to 0.2

--- Kernel.py
+++ Kernel.py
@@ -1,6 +1,6 @@
 def inc():
     print("=========================")
     print("        Sh!tOS")
-    print("      Versão 0.1")
+    print("      Versão 0.2")
     print("=========================")
     print("Inicializando...")
